a022e0e73e68e94e65e9df8b6318f0ee030e22a9
SihamEl23 <elghazi.siham1101@gmail.com>
2023-07-25 12:35:32 -0700

    Handle the path of a command line

---
+++ handle_path.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+/**
+ * handle_path - Handle the path of a command line.
+ *
+ * @com: A pointer to string representing the command line.
+ *
+ * Return: returns the full path of the command, or NULL on failure.
+ */
+char *handle_path(char *com)
+{
+	char *value = _getenv("PATH");
+	char *token, *full_path;
+	struct stat st;
+
+	token = _strtok(value, ":");
+	while (token != NULL)
+	{
+		full_path = malloc(_strlen(token) + _strlen(com) + 2);
+		if (full_path == NULL)
+		{
+			return (NULL);
+		}
+		_strcpy(full_path, token);
+		_strcat(full_path, "/");
+		_strcat(full_path, com);
+		if (stat(full_path, &st) == 0)
+			return (full_path);
+		free(full_path);
+		token = _strtok(NULL, ":");
+	}
+	return (NULL);
+}
+
